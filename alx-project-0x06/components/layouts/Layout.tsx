bbde8089d6ffbfdd3fbebb24593c7d74f315cc0f
thekuu <127185902+thekuu@users.noreply.github.com>
2025-06-14 23:53:22 +0300

    Include a Redux for global state management

---
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import { LayoutProps } from "@/interface";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
